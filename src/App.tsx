230c2a906d6a1a0fa99b6465e921090b8c768aa1
micimize <rosenthalm93@gmail.com>
2017-12-06 00:32:58 -0600

    some decent styling

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,9 @@ import {
 import Welcome from './welcome/Welcome'
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-  web: 'Save code changes to reload,\n' +
-    '⌥⌘I for dev tools',
+  ios: 'Press Cmd+R to reload, Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload, Shake or press menu button for dev menu',
+  web: 'Save code changes to reload, ⌥⌘I for dev tools',
 });
 
 export default class App extends React.Component<{}> {
@@ -21,12 +18,6 @@ export default class App extends React.Component<{}> {
     return (
       <View style={styles.container}>
         <Welcome/>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
       </View>
     );
   }
@@ -39,14 +30,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+})
--- src/welcome/Welcome.tsx
+++ src/welcome/Welcome.tsx
@@ -6,23 +6,46 @@ import {
 } from 'react-native';
 import { Card, Button } from 'react-native-elements'
 
+function u(){
+}
 
 class Welcome extends React.Component<{}> {
   render() {
     return (
       <Card containerStyle={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
+        <Text style={styles.welcome}> チーズケーキ </Text>
+        <Button
+          buttonStyle={styles.secondaryButton}
+          textStyle={styles.secondaryText}
+          onPress={u}
+          title="I have a key"/>
+        <Button 
+          buttonStyle={styles.primaryButton}
+          onPress={u}
+          title="Generate me a key"/>
       </Card>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  primaryButton: {
+    marginTop: 15,
+    borderColor: 'blue',
+    borderRadius: 3,
+    backgroundColor: 'blue',
+    borderWidth: 1,
+  },
+  secondaryButton: {
+    marginTop: 15,
+    backgroundColor: 'white',
+    borderColor: 'blue',
+    borderRadius: 3,
+    borderWidth: 2,
+  },
+  secondaryText: {
+    color: 'blue',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
