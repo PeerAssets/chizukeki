a314b2071e85e674849a26c6bf3bfe25b07322e0
micimize <rosenthalm93@gmail.com>
2017-12-30 19:41:06 -0600

    more switch experiments

--- src/store/switch-witch.ts
+++ src/store/switch-witch.ts
@@ -1,9 +1,13 @@
+
 type Provider = <V extends string>(v: Variant<V>) => V
+
 namespace Provider {
+
   export function is<V extends string>(v: V | Provider): v is Provider {
     return typeof v === "function"
   }
-  export function from(gen: IterableIterator<string>) {
+
+  function fromGenerator(gen: IterableIterator<string>) {
     return <V extends string>(variant: Variant<V>) => {
       let v = gen.next()
       if (variant.is(v)) {
@@ -12,9 +16,28 @@ namespace Provider {
       throw Error('could not provision')
     }
   }
+
+  type Tokenizer<Source extends any = any> = (value: Source) => IterableIterator<string>
+  type TokenProvider<Source> = {
+    (source: Source): Provider
+    to<Return>(wrapped: (p: Provider) => Return): (source: Source) => Return
+  }
+
+  export function from<Source>(tokenizer: Tokenizer<Source>) {
+    function provider(source: Source){
+      return fromGenerator(tokenizer(source))
+    }
+    let p = <TokenProvider<Source>>provider
+    function to<Return>(wrapped: (p: Provider) => Return){
+      return (source: Source) => wrapped(p(source)) as Return
+    }
+    p.to = to
+    return p
+  }
+
 }
 
-type Block<V extends string, Return> = (v: V) => Return
+type Block<V extends string, Return> = (v: V | Provider) => Return
 namespace Block {
   export function is<V extends string, Return>(r: Return | Block<V, Return>): r is Block<V, Return> {
     return typeof r === "function"
@@ -25,29 +48,22 @@ type Case<V extends string, Return> = Return | Block<V, Return>
 
 type Cases<V extends string, Return> = Record<V, Case<V, Return>>
 
-type Generator = <Source extends any = any>(value: Source) => IterableIterator<string>
 
 
 class Variant<V extends string> {
-  constructor(public options: Array<V>){ }
-  is(v: any): v is V {
+  constructor(public options: Array<V>){ } is(v: any): v is V {
     return this.options.includes(v)
-  }
-  with(provider: Provider){
-    return {
-      switch<Return extends any = any>(cases: Cases<V, Return>) {
-        return (ignore? : any): Return => {
-          let v = provider<V>(this)
-          let r: Case<V, Return> = cases[v]
-          return Block.is(r) ? r(v) : r
-        }
-      }
-    }
   }
   switch<Return extends any = any>(cases: Cases<V, Return>): (v: V) => Return {
     return v => {
       let r: Case<V, Return> = cases[v]
       return Block.is(r) ? r(v) : r
+    }
+  }
+  pswitch<Return extends any = any>(cases: Cases<V, Return>): (provider: Provider) => Return {
+    return provider => {
+      let r: Case<V, Return> = cases[provider<V>(this)]
+      return Block.is(r) ? r(provider) : r
     }
   }
 }
@@ -58,65 +74,20 @@ function* slugConsumer(slug: string){
   }
 }
 
-
-let User = new Variant(['login', 'logout'])
+let User = new Variant(['login'])
 let Domains = new Variant(['user'])
-function match(source: string = 'domain/action/state'){
-  let p = Provider.from(slugConsumer('domain/action/state'))
-  Domains.switch({
-    user: User.with(p).switch({
-      login: 'yas',
-      logout: 'yas',
-    })
-  })
-}
+const match = Provider
+    .from(slugConsumer)
+    .to(
+      Domains.pswitch({
+        user: User.pswitch({
+          login: 'login!',
+        })
+      })
+    )
 
-let structure = {}
-
-
-namespace Variant {
-  type Nav = <Source, Token extends string>(value: Source) => IterableIterator<Token>
-//type Navigator = {
-//  <N extends Nav = Nav>(caseDict: N),
-//  with<Source>()
-//  // All we need for a partial alternative is type casting
-//  partial<Return extends any = any>(caseDict: Partial<CaseDict<Return>> & Default<Return>)
-//}
-  class Navigator<Source, N extends Nav> {
-    constructor(public navigator: N){}
-    consume(source: Source){
-
-    }
-    with(){
-
-    }
-  }
-
-  function using<Source, N extends Nav = Nav>(nav: N){
-    function consume(source: Source) {
-    }
-      let S = <DictSwitch>DictSwitch
-      S.partial = <DictSwitch['partial']>DictSwitch
-
-    }
-  }
-
-  function consume(: string){}
-
-    let S = <DictSwitch>DictSwitch
-    S.partial = <DictSwitch['partial']>DictSwitch
-
-}
-
-function* navigator(: ()){
-
-}
-
-namespace Slug {
-  function consumer(action){
-
-  }
-}
+let foo = 'user/login'
+let s = match(foo)
 
 let v = new Variant(['a', 'b', 'c'])
 v.switch({
